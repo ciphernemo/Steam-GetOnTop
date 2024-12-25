7a86f68fe5eebef94097db31df05ed331fc556e4
cipher nemo <ciphernemo@users.noreply.github.com>
2024-12-25 15:24:27 -0500

    Update SteamTools.psm1

--- Modules/SteamTools/SteamTools.psm1
+++ Modules/SteamTools/SteamTools.psm1
@@ -1,142 +1,162 @@
 Function ConvertFrom-VDF {
-    <# 
- .Synopsis 
-     Reads a Valve Data File (VDF) formatted string into a custom object.
+	<# 
+.Synopsis 
+	Reads a Valve Data File (VDF) formatted string into a custom object.
 
- .Description 
-     The ConvertFrom-VDF cmdlet converts a VDF-formatted string to a custom object (PSCustomObject) that has a property for each field in the VDF string. VDF is used as a textual data format for Valve software applications, such as Steam.
+.Description 
+	The ConvertFrom-VDF cmdlet converts a VDF-formatted string to a custom object (PSCustomObject) that has a property for each field in the VDF string. VDF is used as a textual data format for Valve software applications, such as Steam.
 
- .Parameter InputObject
-     Specifies the VDF strings to convert to PSObjects. Enter a variable that contains the string, or type a command or expression that gets the string. 
+.Parameter InputObject
+	Specifies the VDF strings to convert to PSObjects. Enter a variable that contains the string, or type a command or expression that gets the string. 
 
- .Example 
-     $vdf = ConvertFrom-VDF -InputObject (Get-Content ".\SharedConfig.vdf")
+.Example 
+	$vdf = ConvertFrom-VDF -InputObject (Get-Content ".\SharedConfig.vdf")
 
-     Description 
-     ----------- 
-     Gets the content of a VDF file named "SharedConfig.vdf" in the current location and converts it to a PSObject named $vdf
+	Description 
+	----------- 
+	Gets the content of a VDF file named "SharedConfig.vdf" in the current location and converts it to a PSObject named $vdf
 
- .Inputs 
-     System.String
+.Inputs 
+	System.String
 
- .Outputs 
-     PSCustomObject
+.Outputs 
+	PSCustomObject
 
- 
- #>
-    param
-    (
+
+#>
+	param
+	(
 		[Parameter(Position=0, Mandatory=$true)]
 		[ValidateNotNullOrEmpty()]
-        [System.String[]]$InputObject
+		[System.String[]]$InputObject
 	)
-    process
-    {
-        $root = New-Object -TypeName PSObject
-        $chain = [ordered]@{}
-        $depth = 0
-        $parent = $root
-        $element = $null
+	process
+	{
+		$root = New-Object -TypeName PSObject
+		$chain = [ordered]@{}
+		$depth = 0
+		$parent = $root
+		$element = $null
 		
-        ForEach ($line in $InputObject)
-        {
-            $quotedElements = (Select-String -Pattern '(?<=")([^\"\t\s]+\s?)+(?=")' -InputObject $line -AllMatches).Matches
-    
-            if ($quotedElements.Count -eq 1) # Create a new (sub) object
-            {
-                $element = New-Object -TypeName PSObject
-                Add-Member -InputObject $parent -MemberType NoteProperty -Name $quotedElements[0].Value -Value $element
-            }
-            elseif ($quotedElements.Count -eq 2) # Create a new String hash
-            {
-                Add-Member -InputObject $element -MemberType NoteProperty -Name $quotedElements[0].Value -Value $quotedElements[1].Value
-            }
-            elseif ($line -match "{")
-            {
-                $chain.Add($depth, $element)
-                $depth++
-                $parent = $chain.($depth - 1) # AKA $element
-                
-            }
-            elseif ($line -match "}")
-            {
-                $depth--
-                $parent = $chain.($depth - 1)
-				$element = $parent
-                $chain.Remove($depth)
-            }
-            else # Comments etc
-            {
-            }
-        }
+		ForEach ($line in $InputObject)
+		{
+			# Make one to two matches per line of quoted sections, separate by tabs only when next to quote marks,
+			#	match empty and single character quoted sections, include escaped quotes \" in matches but don't make separate matches for \"",
+			#	and include leading and trailing quote marks for safety as they will be removed when converting back to VDF,
+			$quotedElements = (Select-String -Pattern '(?<=^|\t|{|\n)"((?:[^"\\]|\\[\\"])*)"(?=\t|\n|$|})' -InputObject $line -AllMatches).Matches
+	
+			if ($quotedElements.Count -eq 1) # Create a new (sub) object
+			{
+				$element = New-Object -TypeName PSObject
+				Add-Member -InputObject $parent -MemberType NoteProperty -Name $quotedElements[0].Value -Value $element
+			}
+			elseif ($quotedElements.Count -eq 2) # Create a new String hash
+			{
+				Add-Member -InputObject $element -MemberType NoteProperty -Name $quotedElements[0].Value -Value $quotedElements[1].Value
+			}
+			elseif ($line -match "{")
+			{
+				$chain.Add($depth, $element)
+				$depth++
+				$parent = $chain.($depth - 1) # AKA $element
+				
+			}
+			elseif ($line -match "}")
+			{
+				$depth--
+				$parent = $chain.($depth - 1)
+		$element = $parent
+				$chain.Remove($depth)
+			}
+			else # Comments etc
+			{
+			}
+		}
 
-        return $root
-    }
-    
+		return $root
+	}
+	
+}
+
+Function Format-MemberString
+{
+	param
+	(
+		[parameter(Mandatory = $true, Position = 0)]
+		[string]$myMember
+	)
+	return (($myMember).Substring(1, $myMember.Length - 2))
 }
 
 Function ConvertTo-VDF
 {
-    <# 
- .Synopsis 
-     Converts a custom object into a Valve Data File (VDF) formatted string.
+	<# 
+.Synopsis 
+	Converts a custom object into a Valve Data File (VDF) formatted string.
 
- .Description 
-     The ConvertTo-VDF cmdlet converts any object to a string in Valve Data File (VDF) format. The properties are converted to field names, the field values are converted to property values, and the methods are removed.
+.Description 
+	The ConvertTo-VDF cmdlet converts any object to a string in Valve Data File (VDF) format. The properties are converted to field names, the field values are converted to property values, and the methods are removed.
 
- .Parameter InputObject
-     Specifies PSObject to be converted into VDF strings.  Enter a variable that contains the object. You can also pipe an object to ConvertTo-Json.
+.Parameter InputObject
+	Specifies PSObject to be converted into VDF strings.  Enter a variable that contains the object. You can also pipe an object to ConvertTo-Json.
 
- .Example 
-     ConvertTo-VDF -InputObject $VDFObject | Out-File ".\SharedConfig.vdf"
+.Example 
+	ConvertTo-VDF -InputObject $VDFObject | Out-File ".\SharedConfig.vdf"
 
-     Description 
-     ----------- 
-     Converts the PS object to VDF format and pipes it into "SharedConfig.vdf" in the current directory
+	Description 
+	----------- 
+	Converts the PS object to VDF format and pipes it into "SharedConfig.vdf" in the current directory
 
- .Inputs 
-     PSCustomObject
+.Inputs 
+	PSCustomObject
 
- .Outputs 
-     System.String
+.Outputs 
+	System.String
 
- 
- #>
-    param
-    (
+
+#>
+	param
+	(
 		[Parameter(Position=0, Mandatory=$true, ValueFromPipeline=$true)]
 		[ValidateNotNullOrEmpty()]
 		[PSObject]$InputObject,
 
-        [Parameter(Position=1, Mandatory=$false)]
+		[Parameter(Position=1, Mandatory=$false)]
 		[int]$Depth = 0
 	)
-    process
-    {
-        $output = ""
-        
-        ForEach ( $property in ($InputObject.psobject.Members | Where {($_.MemberType -eq "NoteProperty") -and ($_.TypeNameOfValue -eq "System.String")}) )
-        {
-            $output += ("`t" * $Depth) + "`"" + $property.Name + "`"`t`"" + $InputObject.($property.Name) + "`"`n"
-        }
-        ForEach ( $property in ($InputObject.psobject.Members | Where {($_.MemberType -eq "NoteProperty") -and ($_.TypeNameOfValue -eq "System.Management.Automation.PSCustomObject")}) )
-        {
-            $element = $InputObject.($property.Name)
-            $output += ("`t" * $Depth) + "`"" + $property.Name + "`"`n"
-            $output += ("`t" * $Depth) + "{`n"
-            $Depth++
-            $output += ConvertTo-VDF -InputObject $element -Depth $Depth
-            $Depth--
-            $output +=  ("`t" * $Depth) + "}"
-            if ($Depth -gt 0)
-            {
-                $output += "`n"
-            }
-        }
-    
-        return $output
-    }
-    
+	process
+	{
+		$output = [String]::Empty
+		$members = $InputObject.PSObject.Members | Where-Object { $_.MemberType -eq "NoteProperty" }
+		for ($i = 0; $i -lt $members.Count; $i++)
+		{
+			$member = $members[$i]
+			if ($member.TypeNameOfValue -eq "System.String")
+			{
+				$tabindent = "`t" * $Depth
+				$m1 = Format-MemberString $member.Name
+				$m2 = Format-MemberString ($InputObject.($member.Name))
+				$output += $tabindent + "`"" + $m1 + "`"`t`t`"" + $m2 + "`"`n"
+			}
+			elseif ($member.TypeNameOfValue -eq "System.Management.Automation.PSCustomObject")
+			{
+				$tabindent = "`t" * $Depth
+				$element = $InputObject.($member.Name)
+				$output += $tabindent + "`"" + (Format-MemberString $member.Name) + "`"`n"
+				$output += $tabindent + "{`n"
+				$Depth++
+				$output += ConvertTo-VDF -InputObject $element -Depth $Depth
+				$Depth--
+				$output += $tabindent + "}"
+				if ($Depth -gt 0)
+				{
+					$output += "`n"
+				}
+			}
+		}
+		return $output
+	}
+	
 }
 
 Function Get-SteamPath {
