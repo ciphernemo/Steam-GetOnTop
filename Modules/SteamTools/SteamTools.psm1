17db9cee6fdc217574c22a5094ce35b3f089bb1b
cipher nemo <ciphernemo@users.noreply.github.com>
2025-01-18 13:07:39 -0500

    Update SteamTools.psm1

--- Modules/SteamTools/SteamTools.psm1
+++ Modules/SteamTools/SteamTools.psm1
@@ -1,180 +1,218 @@
-Function ConvertFrom-VDF {
-	<# 
-.Synopsis 
-	Reads a Valve Data File (VDF) formatted string into a custom object.
-
-.Description 
-	The ConvertFrom-VDF cmdlet converts a VDF-formatted string to a custom object (PSCustomObject) that has a property for each field in the VDF string. VDF is used as a textual data format for Valve software applications, such as Steam.
-
-.Parameter InputObject
-	Specifies the VDF strings to convert to PSObjects. Enter a variable that contains the string, or type a command or expression that gets the string. 
-
-.Example 
-	$vdf = ConvertFrom-VDF -InputObject (Get-Content ".\SharedConfig.vdf")
-
-	Description 
-	----------- 
-	Gets the content of a VDF file named "SharedConfig.vdf" in the current location and converts it to a PSObject named $vdf
-
-.Inputs 
-	System.String
-
-.Outputs 
-	PSCustomObject
-
-
-#>
-	param
-	(
-		[Parameter(Position=0, Mandatory=$true)]
-		[ValidateNotNullOrEmpty()]
-		[System.String[]]$InputObject
-	)
-	process
-	{
-		$root = New-Object -TypeName PSObject
-		$chain = [ordered]@{}
-		$depth = 0
-		$parent = $root
-		$element = $null
-		
-		ForEach ($line in $InputObject)
-		{
-			# Make one to two matches per line of quoted sections, separate by tabs only when next to quote marks,
-			#	match empty and single character quoted sections, include escaped quotes \" in matches but don't make separate matches for \"",
-			#	and include leading and trailing quote marks for safety as they will be removed when converting back to VDF,
-			$quotedElements = (Select-String -Pattern '(?<=^|\t|{|\n)"((?:[^"\\]|\\[\\"])*)"(?=\t|\n|$|})' -InputObject $line -AllMatches).Matches
-	
-			if ($quotedElements.Count -eq 1) # Create a new (sub) object
-			{
-				$element = New-Object -TypeName PSObject
-				Add-Member -InputObject $parent -MemberType NoteProperty -Name $quotedElements[0].Value -Value $element
-			}
-			elseif ($quotedElements.Count -eq 2) # Create a new String hash
-			{
-				Add-Member -InputObject $element -MemberType NoteProperty -Name $quotedElements[0].Value -Value $quotedElements[1].Value
-			}
-			elseif ($line -match "{")
-			{
-				$chain.Add($depth, $element)
-				$depth++
-				$parent = $chain.($depth - 1) # AKA $element
-				
-			}
-			elseif ($line -match "}")
-			{
-				$depth--
-				$parent = $chain.($depth - 1)
-		$element = $parent
-				$chain.Remove($depth)
-			}
-			else # Comments etc
-			{
-			}
-		}
-
-		return $root
-	}
-	
-}
-
-Function Format-MemberString
+#formats a string to trim leading and trailing quote marks
+function Format-MemberString
 {
 	param
 	(
-		[parameter(Mandatory = $true, Position = 0)]
+		[parameter(Mandatory = $true)]
 		[string]$myMember
 	)
 	return (($myMember).Substring(1, $myMember.Length - 2))
 }
 
-Function ConvertTo-VDF
+#formats a path to replace all forward slashes with back slashes and return its proper capitalization
+function Format-ProperPath
 {
-	<# 
-.Synopsis 
-	Converts a custom object into a Valve Data File (VDF) formatted string.
-
-.Description 
-	The ConvertTo-VDF cmdlet converts any object to a string in Valve Data File (VDF) format. The properties are converted to field names, the field values are converted to property values, and the methods are removed.
-
-.Parameter InputObject
-	Specifies PSObject to be converted into VDF strings.  Enter a variable that contains the object. You can also pipe an object to ConvertTo-Json.
-
-.Example 
-	ConvertTo-VDF -InputObject $VDFObject | Out-File ".\SharedConfig.vdf"
-
-	Description 
-	----------- 
-	Converts the PS object to VDF format and pipes it into "SharedConfig.vdf" in the current directory
-
-.Inputs 
-	PSCustomObject
-
-.Outputs 
-	System.String
-
-
-#>
 	param
 	(
-		[Parameter(Position=0, Mandatory=$true, ValueFromPipeline=$true)]
-		[ValidateNotNullOrEmpty()]
-		[PSObject]$InputObject,
-
-		[Parameter(Position=1, Mandatory=$false)]
-		[int]$Depth = 0
+		[parameter(Mandatory = $true)]
+		[string]$Path
 	)
-	process
+	$Path = $Path.Replace('/', '\')
+	$properPath = [String]::Empty
+	if (!((Test-Path $Path -PathType Leaf) -or (Test-Path $Path -PathType Container)))
 	{
-		$output = [String]::Empty
-		$members = $InputObject.PSObject.Members | Where-Object { $_.MemberType -eq "NoteProperty" }
-		for ($i = 0; $i -lt $members.Count; $i++)
+		return $Path
+	}
+	foreach ($branch in $Path.Split("\"))
+	{
+		if ($properPath -eq "")
 		{
-			$member = $members[$i]
-			if ($member.TypeNameOfValue -eq "System.String")
+			$properPath = $branch.ToUpper() + "\"
+			continue
+		}
+		$properPath = [System.IO.Directory]::GetFileSystemEntries($properPath, $branch)[0];
+	}
+	return $properPath;
+}
+
+#parses a VDF and converts it to a custom object
+#	example: $vdf = ConvertFrom-VDF -source (Get-Content "C:\localconfig.vdf")
+function ConvertFrom-VDF
+{
+	param
+	(
+		[parameter(Mandatory = $true)]
+		[ValidateNotNullOrEmpty()]
+		[System.String[]]$Source
+	)
+	$root = New-Object -TypeName PSObject
+	$chain = [ordered]@{}
+	$treeDepth = 0
+	$parent = $root
+	$element = $null
+	$i = 0
+	foreach ($line in $Source)
+	{
+		#make one to two matches per line of quoted sections, separate by tabs only when next to quote marks,
+		#	match empty and single character quoted sections,
+		#	include escaped quotes \" in matches but don't make separate matches for \"",
+		#	and include leading and trailing quote marks for safety as they will be removed when converting back to VDF
+		$pattern = '(?<=^|\t|{|\n)"((?:[^"\\]|\\[\\"])*)"(?=\t|\n|$|})'
+		$quotedElements = (Select-String -Pattern $pattern -InputObject $line -AllMatches).Matches
+		#create a new sub object
+		if ($quotedElements.Count -eq 1)
+		{
+			$element = New-Object -TypeName PSObject
+			Add-Member -InputObject $parent -MemberType NoteProperty -Name $quotedElements[0].Value -Value $element
+		}
+		#create a new string hash
+		elseif ($quotedElements.Count -eq 2)
+		{
+			Add-Member -InputObject $element -MemberType NoteProperty -Name $quotedElements[0].Value -Value $quotedElements[1].Value
+		}
+		elseif ($line -match "{")
+		{
+			$chain.Add($treeDepth, $element)
+			$parent = $chain.$treeDepth
+			$treeDepth++
+		}
+		elseif ($line -match "}")
+		{
+			$treeDepth--
+			$treeDepthLower = $treeDepth - 1
+			$parent = $chain.$treeDepthLower
+			$element = $parent
+			$chain.Remove($treeDepth)
+		}
+		$i++
+	}
+	return $root
+}
+
+#converts an object to a VDF file
+#	example: [System.IO.File]::WriteAllLines($vdfFile, (ConvertTo-VDF -Source $vdfObject))
+function ConvertTo-VDF
+{
+	param
+	(
+		[parameter(Mandatory = $true, Position = 0)]
+		[PSObject]$Source,
+		[parameter(Mandatory = $false, Position = 1)]
+		[int]$treeDepth = 0
+	)
+	$output = [String]::Empty
+	$members = $Source.PSObject.Members | Where-Object { $_.MemberType -eq "NoteProperty" }
+	for ($i = 0; $i -lt $members.Count; $i++)
+	{
+		$member = $members[$i]
+		if ($member.TypeNameOfValue -eq "System.String")
+		{
+			$tabIndent = "`t" * $treeDepth
+			$m1 = Format-MemberString $member.Name
+			$m2 = Format-MemberString ($Source.($member.Name))
+			$output += $tabIndent + "`"" + $m1 + "`"`t`t`"" + $m2 + "`"`n"
+		}
+		elseif ($member.TypeNameOfValue -eq "System.Management.Automation.PSCustomObject")
+		{
+			$tabIndent = "`t" * $treeDepth
+			$element = $Source.($member.Name)
+			$output += $tabIndent + "`"" + (Format-MemberString $member.Name) + "`"`n"
+			$output += $tabIndent + "{`n"
+			$treeDepth++
+			$output += ConvertTo-VDF -Source $element -treeDepth $treeDepth
+			$treeDepth--
+			$output += $tabIndent + "}"
+			if ($treeDepth -gt 0)
 			{
-				$tabindent = "`t" * $Depth
-				$m1 = Format-MemberString $member.Name
-				$m2 = Format-MemberString ($InputObject.($member.Name))
-				$output += $tabindent + "`"" + $m1 + "`"`t`t`"" + $m2 + "`"`n"
+				$output += "`n"
 			}
-			elseif ($member.TypeNameOfValue -eq "System.Management.Automation.PSCustomObject")
+		}
+	}
+	return $output
+}
+
+#finds the Steam install path
+function Get-SteamPath
+{
+	#search for Steam in registry keys
+	[string[]]$hive = @("HKCU:", "HKLM:")
+	foreach ($h in $hive)
+	{
+		try
+		{
+			$key = "$h\Software\Valve\Steam\"
+			if (Test-Path -Path $key)
 			{
-				$tabindent = "`t" * $Depth
-				$element = $InputObject.($member.Name)
-				$output += $tabindent + "`"" + (Format-MemberString $member.Name) + "`"`n"
-				$output += $tabindent + "{`n"
-				$Depth++
-				$output += ConvertTo-VDF -InputObject $element -Depth $Depth
-				$Depth--
-				$output += $tabindent + "}"
-				if ($Depth -gt 0)
+				$steam = Format-ProperPath ((Get-ItemProperty $key).SteamPath)
+				if (Test-Path -Path $steam) { return $steam }
+			}
+		}
+		catch
+		{
+			continue
+		}
+	}
+	#test for default install path
+	$pfx86 = "${Env:ProgramFiles(x86)}"
+	if (Test-Path -Path "$pfx86\Steam\steam.exe")
+	{
+		return "$pfx86\Steam\steam.exe"
+	}
+	#search drives for steam.exe file
+	else
+	{
+		Write-Host "Steam client not within the Registry or default location. Searching your drives for Steam..."
+		#set up system drive exclusions for search
+		[string[]]$paths = @($Env:SystemRoot, $Env:ProgramData, $Env:TEMP, "$Env:SystemRoot\Temp", "$Env:SystemDrive\Recovery")
+		$paths += ("$Env:USERPROFILE\GoogleDrive", "$Env:USERPROFILE\Box")
+		if ($Env:OneDrive) { $paths += $Env:OneDrive }
+		#get all drives on the system
+		[string[]]$driveLetters = Get-PSDrive | Select-Object -ExpandProperty "Name" | Select-String -Pattern '^[a-z]$'
+		[string[]]$results = @()
+		foreach ($d in $driveLetters)
+		{
+			$d = $d + ":\"
+			Write-Host "Searching $d drive..."
+			#recursively search a drive and add matches to results
+			if ($items = Get-ChildItem -Path $d -Filter "steam.exe" -Exclude $paths -Recurse)
+			{
+				foreach ($item in $items)
 				{
-					$output += "`n"
+					$results += $item.FullName
 				}
 			}
 		}
-		return $output
+		if ($results.Count -gt 0)
+		{
+			return $results
+		}
+		else
+		{
+			#no results found
+			Write-Host "Steam not found on the local system. Exiting..."
+			Start-Sleep -Seconds 6
+			exit
+		}
 	}
-	
 }
 
-Function Get-SteamPath {
-	return "$((Get-ItemProperty HKCU:\Software\Valve\Steam\).SteamPath)".Replace('/','\')
-}
 
-Function Get-SteamID64 {
-param(
-	[Parameter(Position=0, Mandatory=$true)]
-	[System.Int32]$SteamID3
-)
-	if (($SteamID3 % 2) -eq 0) {
+Function Get-SteamID64
+{
+	param
+ 	(
+		[Parameter(Position=0, Mandatory=$true)]
+		[System.Int32]$SteamID3
+	)
+	if (($SteamID3 % 2) -eq 0)
+ 	{
 		$Y = 0;
 		$Z = ($SteamID3 / 2);
-	} else {
+	}
+	else
+	{
 		$Y = 1;
 		$Z = (($SteamID3 - 1) / 2);
 	}
-
 	return "7656119$(($Z * 2) + (7960265728 + $Y))"
 }
