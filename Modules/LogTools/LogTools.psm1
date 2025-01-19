37ce7eda5e107b5579b985b75b94d01a8b31f59c
cipher nemo <ciphernemo@users.noreply.github.com>
2025-01-19 00:35:39 -0500

    Update LogTools.psm1
    Minor formatting changes

--- Modules/LogTools/LogTools.psm1
+++ Modules/LogTools/LogTools.psm1
@@ -1,5 +1,9 @@
-function Write-LogHeader { 
-	param (
+# --------- Functions ---------
+
+function Write-LogHeader
+{
+	param
+	(
 		[Parameter(Position=0, Mandatory=$true)]
 		[String]$InputObject
 	)
@@ -10,8 +14,10 @@ function Write-LogHeader {
 	Out-File -FilePath $log -Append -InputObject "==========================================================================="
 }
 
-function Write-LogFooter {
-	param (
+function Write-LogFooter
+{
+	param
+	(
 		[Parameter(Position=0, Mandatory=$true)]
 		[String]$InputObject
 	)
@@ -21,41 +27,51 @@ function Write-LogFooter {
 	Out-File -FilePath $log -Append -InputObject "==========================================================================="
 }
 
-function Write-Log {
-	param (
+function Write-Log
+{
+	param
+	(
 		[Parameter(Position=0, Mandatory=$true)]
-		[String]$InputObject
-		,
+		[String]$InputObject,
 		[Parameter(Mandatory=$false)]
 		[ValidateSet("Error", "Warning", "Standard", "Verbose", "Debug")] 
 		[String]$MessageLevel = "Standard"
 	)
-	$symbol = switch ($MessageLevel) {
-		"Error" 	{ "!" }
+	$symbol = switch ($MessageLevel)
+	{
+		"Error"		{ "!" }
 		"Warning"	{ "^" }
-		"Standard" 	{ "." }
-		"Verbose" 	{ "+" }
-		"Debug" 	{ "?" }
+		"Standard"	{ "." }
+		"Verbose"	{ "+" }
+		"Debug"		{ "?" }
 	}
 	
-	switch ($MessageLevel) {
+	switch ($MessageLevel)
+	{
 		"Error" 	{$foregroundColour = "Red";						$backgroundColour = "Black"}
-		"Warning"   {$foregroundColour = "Yellow";					$backgroundColour = "Black"}
+		"Warning"	{$foregroundColour = "Yellow";					$backgroundColour = "Black"}
 		default 	{$foregroundColour = $defaultForegroundColour;	$backgroundColour = $defaultBackgroundColour}
 	}
-	if ($MessageLevel -eq "Debug") {
-		if ($Level -eq "Debug") {
+	if ($MessageLevel -eq "Debug")
+	{
+		if ($Level -eq "Debug")
+		{
 			Out-File -FilePath $log -Append -InputObject "$(Get-Date -Format "hh:mm:ss tt") [$($symbol)] : $($InputObject)"
 			Write-Host $InputObject -ForegroundColor $foregroundColour -BackgroundColor $backgroundColour
 		}
 	}
-	elseif ($MessageLevel -eq "Verbose") {
-		if ( ($Level -eq "Debug") -or ($Level -eq "Verbose") ) {
+	elseif ($MessageLevel -eq "Verbose")
+	{
+		if (($Level -eq "Debug") -or ($Level -eq "Verbose"))
+		{
 			Out-File -FilePath $log -Append -InputObject "$(Get-Date -Format "hh:mm:ss tt") [$($symbol)] : $($InputObject)"
 			Write-Host $InputObject -ForegroundColor $foregroundColour -BackgroundColor $backgroundColour
 		}
-	} else {
-		if ($Level -ne "None") {
+	}
+	else
+	{
+		if ($Level -ne "None")
+		{
 			Out-File -FilePath $log -Append -InputObject "$(Get-Date -Format "hh:mm:ss tt") [$($symbol)] : $($InputObject)"
 			Write-Host $InputObject -ForegroundColor $foregroundColour -BackgroundColor $backgroundColour
 		}
@@ -63,16 +79,20 @@ function Write-Log {
 	
 }
 
-function Set-LogPath {
-	param (
+function Set-LogPath
+{
+	param
+	(
 		[Parameter(Position=0, Mandatory=$true)]
 		[String]$LogPath
 	)
 	$script:log = $LogPath
 }
 
-function Set-LogLevel {
-	param (
+function Set-LogLevel
+{
+	param
+	(
 		[Parameter(Position=0, Mandatory=$true)]
 		[ValidateSet("None", "Standard", "Verbose", "Debug")]
 		[String]$LogLevel
@@ -80,6 +100,7 @@ function Set-LogLevel {
 	$script:Level = $LogLevel
 }
 
+# --------- Main ---------
+
 $defaultForegroundColour = (Get-Host).UI.RawUI.ForegroundColor
 $defaultBackgroundColour = (Get-Host).UI.RawUI.BackgroundColor
- 
